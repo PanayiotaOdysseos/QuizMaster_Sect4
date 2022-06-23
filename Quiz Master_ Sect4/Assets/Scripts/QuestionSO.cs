e00b931403f21cdb51a09ed4d4c6add17c911abb
PanayiotaOdysseos <odysseospan@gmail.com>
2022-06-23 14:09:31 +0300

    commit

---
+++ Quiz Master_ Sect4/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : MonoBehaviour
+{
+    //[SerializeField] string question = "Enter new question text here";
+}
