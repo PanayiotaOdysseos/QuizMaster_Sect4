828994c3c46c3ea3334e8a0db3abb1a00c688222
PanayiotaOdysseos <odysseospan@gmail.com>
2022-06-23 18:04:19 +0300

    update

---
+++ Quiz Master_ Sect4/Assets/Scripts/Timer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float timerValue;
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+
+    public bool isAnsweringQuestion = false;
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion)
+        {
+            if(timerValue<=0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+                
+            }
+
+        }
+        else
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+
+            }
+
+        }
+      
+        /*  if (timerValue <=0 && isAnsweringQuestion == false)
+        {
+            timerValue = timeToCompleteQuestion;
+            isAnsweringQuestion = true;
+        }
+        else if (timerValue <= 0 && isAnsweringQuestion == true)
+        {
+            timerValue = timeToShowCorrectAnswer;
+            isAnsweringQuestion = false;
+
+        } */
+
+        Debug.Log(timerValue);
+    }
+}
